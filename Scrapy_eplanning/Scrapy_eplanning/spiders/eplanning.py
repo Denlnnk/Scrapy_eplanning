532644878b1786849b4a8d1fcc649c7b29baad34
denis <denlnnk2014@gmail.com>
2022-12-04 14:50:59 +0200

    Implemented parse_country

--- Scrapy_eplanning/Scrapy_eplanning/spiders/eplanning.py
+++ Scrapy_eplanning/Scrapy_eplanning/spiders/eplanning.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy import FormRequest
 
 
 class EplanningSpider(scrapy.Spider):
@@ -7,4 +8,18 @@ class EplanningSpider(scrapy.Spider):
     start_urls = ['http://eplanning.ie/']
 
     def parse(self, response, **kwargs):
+        counties_url = response.xpath('//td/a/@href').get()
+        # for url in counties_url:
+        #     if url == '#':
+        #         pass
+        #     else:
+        yield scrapy.Request(counties_url, callback=self.parse_country)
+
+    def parse_country(self, response):
+        received_app_url = response.xpath(
+            '//span[@class="glyphicon glyphicon-inbox btn-lg"]/following-sibling::a/@href').get()
+        absolute_received_app_url = response.urljoin(received_app_url)
+        yield scrapy.Request(absolute_received_app_url, callback=self.parse_form)
+
+    def parse_form(self, response):
         pass
