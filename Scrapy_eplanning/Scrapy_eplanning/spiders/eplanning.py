5417fc695e630ce86e11398d33fa444a85ecdb21
denis <denlnnk2014@gmail.com>
2022-12-04 14:51:36 +0200

    Implemented parse_pages

--- Scrapy_eplanning/Scrapy_eplanning/spiders/eplanning.py
+++ Scrapy_eplanning/Scrapy_eplanning/spiders/eplanning.py
@@ -22,4 +22,24 @@ class EplanningSpider(scrapy.Spider):
         yield scrapy.Request(absolute_received_app_url, callback=self.parse_form)
 
     def parse_form(self, response):
-        pass
+        yield FormRequest.from_response(response,
+                                        formdata={
+                                            'RdoTimeLimit': '42'
+                                        },
+                                        dont_filter=True,
+                                        formxpath='(//form)[2]',
+                                        callback=self.parse_pages
+                                        )
+
+    def parse_pages(self, response):
+        file_number_links = response.xpath('//tr/td/a/@href').getall()
+        for link in file_number_links:
+            absolute_link = response.urljoin(link)
+            yield scrapy.Request(absolute_link, callback=self.parse_details)
+
+        next_page = response.xpath('//a[@rel="next"]/@href').get()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse_pages)
+
+    def parse_details(self, response):
+        self.logger.info(response)
