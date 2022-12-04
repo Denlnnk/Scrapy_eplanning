8b179839bfe74e0f5407d006ebfa204ae834e168
denis <denlnnk2014@gmail.com>
2022-12-04 13:59:56 +0200

    Initial commit

---
+++ Scrapy_eplanning/Scrapy_eplanning/spiders/eplanning.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class EplanningSpider(scrapy.Spider):
+    name = 'eplanning'
+    allowed_domains = ['eplanning.ie']
+    start_urls = ['http://eplanning.ie/']
+
+    def parse(self, response, **kwargs):
+        pass
